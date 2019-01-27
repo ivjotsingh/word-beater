66ac686e7887444d84493a864dc9ce2065720c54
ivjot singh <ivjot@supply.ai>
2019-01-27 22:51:56 +0530

    set interval function added for the countdown

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
 
+let time = 5;
+let isPlaying;
 
 const words = [
     'hat',
@@ -40,4 +42,18 @@ const words = [
 function init(){
 
     currentWord.innerHTML = 'hey'
+    setInterval(countdown, 1000)
 };
+
+function countdown() {
+    // Make sure time is not run out
+    if (time > 0) {
+      // Decrement
+      time--;
+    } else if (time === 0) {
+      // Game is over
+      isPlaying = false;
+    }
+    // Show time
+    timeDisplay.innerHTML = time;
+  }
